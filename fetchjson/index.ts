f4a9d42ec5e2a7f3bb2d7db5552e89df555bac84
idtr <ivanlozmar@gmail.com>
2020-04-18 16:53:48 +0200

    Extracted console.log statement into a separate helper function and add type annotations to arguments in it

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -15,9 +15,14 @@ axios.get(url).then((response) => {
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+// Defining a function and setting it up as un arrow function
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The Todo with ID: ${id}
     Has a title of: ${title}
     Is it finished? ${completed}
   `);
-});
+};
