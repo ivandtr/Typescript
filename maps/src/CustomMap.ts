4f4bdace2e2ee69c4633c0e8fef1009801afe4f4
idtr <ivanlozmar@gmail.com>
2020-04-19 15:34:39 +0200

    Added functions in CustomMap to add markers

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,5 +1,8 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
-  googleMap: google.maps.Map;
+  private googleMap: google.maps.Map;
 
   constructor(divId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
@@ -7,6 +10,26 @@ export class CustomMap {
       center: {
         lat: 0,
         lng: 0,
+      },
+    });
+  }
+
+  addUserMarker(user: User) {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company) {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
       },
     });
   }
