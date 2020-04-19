dc263ccea50eae845e4abb0b0103d62ea0848d67
idtr <ivanlozmar@gmail.com>
2020-04-19 17:42:30 +0200

    Added infowindows to markers

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,11 +1,13 @@
 import { User } from './User';
 import { Company } from './Company';
 
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
+  color: string;
 }
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -20,13 +22,21 @@ export class CustomMap {
     });
   }
 
-  addMarker(mappable: Mappable) {
-    new google.maps.Marker({
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
