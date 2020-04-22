233a5d6de86c3454fe00648f6d54f8c12f069c41
idtr <ivanlozmar@gmail.com>
2020-04-22 16:36:00 +0200

    Added fetch and save methods to User class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Ivan', age: 32 });
-console.log(user.get('age'));
-console.log(user.get('name'));
-console.log(user);
-user.on('click', () => {
-  console.log('click works!!!');
-});
+const user2 = new User({ name: 'Ivan', age: 32 });
+// console.log(user.get('age'));
 
-user.trigger('click');
+user2.save();
