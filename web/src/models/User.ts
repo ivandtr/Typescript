c22484d47c2e7e9d7e5c27ccbce25e0554dfda79
idtr <ivanlozmar@gmail.com>
2020-04-23 12:51:47 +0200

    Implemented get, on and trigger as pass through methods in User class

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
   id?: number;
@@ -12,8 +13,21 @@ const url = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(url);
-}
+  public attributes: Attributes<UserProps>;
 
-// const handlers = this.events[eventName] || [];
-// handlers.push(callback);
-// this.events[eventName] = handlers;
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+}
