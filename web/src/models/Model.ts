7038887e512466e6d6e7cb8a0eddf893cd4b76a4
idtr <ivanlozmar@gmail.com>
2020-04-24 16:56:10 +0200

    Extrated User method into Model in order to implement inheritance approach

---
+++ web/src/models/Model.ts
@@ -0,0 +1,73 @@
+import { AxiosPromise, AxiosResponse } from 'axios';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
+
+interface HasId {
+  id?: number;
+}
+interface ModelAttributes<T> {
+  get<K extends keyof T>(key: K): T[K];
+  set(update: T): void;
+  getAll(): T;
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+
+type Callback = () => void;
+
+interface Events {
+  on(eventName: string, callback: Callback): void;
+  trigger(eventName: string): void;
+}
+
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private sync: Sync<T>,
+    private events: Events
+  ) {}
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: T): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
+  }
+}
