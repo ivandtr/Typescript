4556228dbe16f6d117b73df5937b8fef87114b9d
idtr <ivanlozmar@gmail.com>
2020-05-03 10:46:26 +0200

    Work in progress

--- web/src/models/Collection.ts
+++ web/src/models/Collection.ts
@@ -20,8 +20,7 @@ export class Collection<T, K> {
       response.data.forEach((object: K) => {
         this.models.push(this.deserialize(object));
       });
+      this.trigger('change');
     });
-
-    this.trigger('change');
   }
 }
